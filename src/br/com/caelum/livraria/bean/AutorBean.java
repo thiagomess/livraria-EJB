b4b629d18da3e8a2eaa5dce965a357c33899f6a8
Thiago Gomes <thiagogomes19@hotmail.com>
2018-08-08 15:09:21 -0300

    Aula 1.
    @Stateless e @Inject

---
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -0,0 +1,31 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.dao.AutorDao;
+import br.com.caelum.livraria.modelo.Autor;
+
+@Model
+public class AutorBean {
+	
+	private Autor autor = new Autor();
+	
+	@Inject //É passado a administração para o EJB Containeir, por isso nao se instancia mais o DAO. (injeção de dependencia)
+	private AutorDao dao;
+	
+	public Autor getAutor() {
+		return autor;
+	}
+	
+	public void cadastra() {
+		this.dao.salva(autor);
+		this.autor = new Autor();
+	}
+	
+	public List<Autor> getAutores() {
+		return this.dao.todosAutores();
+	}
+}
